1d30c4a01ca041debb8c89e36a46bdeb93003493
Robin van Maasakker <robin@dashed.nl>
2026-03-06 11:23:36 +0100

    Update

--- src/DashedEcommerceBolPlugin.php
+++ src/DashedEcommerceBolPlugin.php
@@ -2,11 +2,14 @@
 
 namespace Dashed\DashedEcommerceBol;
 
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Panel;
 use Filament\Contracts\Plugin;
 use Dashed\DashedEcommerceCore\Models\Order;
 use Dashed\DashedEcommerceBol\Filament\Widgets\BolOrderStats;
 use Dashed\DashedEcommerceBol\Filament\Pages\Settings\BolSettingsPage;
+use Filament\Schemas\Components\Section;
 
 class DashedEcommerceBolPlugin implements Plugin
 {
@@ -30,8 +33,32 @@ class DashedEcommerceBolPlugin implements Plugin
             ]);
     }
 
+    public static function builderBlocks(): void
+    {
+        cms()
+            ->builder('productBlocks', [
+                TextInput::make('bol-product-title')
+                    ->label('Bol product titel')
+                    ->helperText('Mogelijke variablen: :name:, :categorie naam:'),
+            ]);
+    }
+
     public function boot(Panel $panel): void
     {
+        cms()->builder('builderBlockClasses', [
+            self::class => 'builderBlocks',
+        ]);
 
+        ecommerce()
+            ->builder('productPriceFields', [
+                'bol_price' => [
+                    'label' => 'Bol prijs',
+                    'helperText' => 'Voorbeeld: 10.25',
+                ],
+                'bol_old_price' => [
+                    'label' => 'Vorige bol prijs (hogere prijs)',
+                    'helperText' => 'Voorbeeld: 14.25',
+                ],
+            ]);
     }
 }
